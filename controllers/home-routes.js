12a8c6fe82f10516f182796e0f0e2283573d0b48
Keri Price <keri.lynne.p@gmail.com>
2021-09-29 16:07:33 -0400

    updated routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,30 +11,48 @@ router.get('/', (req, res) => {
 });
 
 router.get('/location/:id', async (req, res) => {
-    try {
-      const locationData = await Location.findByPk(req.params.id, {
-        include: [
-          {
-            model: Car,
-            attributes: [
-              'id',
-              'year',
-              'make',
-              'model',
-              'price',
-              'color',
-              'image'
-            ],
-          },
-        ],
-      });
-  
-      const location = locationData.get({ plain: true });
-      res.render('location', { location });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+  try {
+    const locationData = await Location.findByPk(req.params.id, {
+      include: [
+        {
+          model: Car,
+          attributes: [
+            'id',
+            'year',
+            'make',
+            'model',
+            'price',
+            'color',
+            'image'
+          ],
+        },
+      ],
+    });
+
+    const location = locationData.get({ plain: true });
+    res.render('location', { location });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/rentals', async (req, res) => {
+  try {
+    const carData = await Car.findAll();
+
+    const cars = carData.map((car) =>
+      car.get({ plain: true })
+    );
+
+    res.render('rentals', {
+      cars,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
