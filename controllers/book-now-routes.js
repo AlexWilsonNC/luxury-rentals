93d0cd9cd779942687243309be45b69b11bbb09e
Keri Price <keri.lynne.p@gmail.com>
2021-10-01 14:13:34 -0400

    added routes for book-now

--- controllers/book-now-routes.js
+++ controllers/book-now-routes.js
@@ -2,7 +2,89 @@ const router = require('express').Router();
 const { Car, Location, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/:city/:car', withAuth, async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const carData = await Car.findAll();
+        const cars = carData.map((car) =>
+            car.get({ plain: true })
+        );
+
+        const locationData = await Location.findAll();
+        const cities = locationData.map((city) =>
+            city.get({ plain: true })
+        );
+
+        res.render('booknow', { cities, cars, logged_in: req.session.logged_in });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/city/:city', withAuth, async (req, res) => {
+    try {
+        const locationData = await Location.findAll();
+        const cities = locationData.map((city) =>
+            city.get({ plain: true })
+        );
+        
+        const choiceData = await Location.findByPk(req.params.city, {
+            include: [
+                {
+                    model: Car,
+                    attributes: [
+                        'id',
+                        'year',
+                        'make',
+                        'model',
+                        'price',
+                        'color',
+                        'image'
+                    ]
+                },
+            ],
+        });
+
+        const cityChoice = choiceData.get({ plain: true });
+        const cars = cityChoice.cars;
+        res.render('booknow', { cities, cars, cityChoice, logged_in: req.session.logged_in });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/car/:car', withAuth, async (req, res) => {
+    try {
+        const carData = await Car.findAll();
+        const cars = carData.map((car) =>
+            car.get({ plain: true })
+        );
+        
+        const carData = await Car.findByPk(req.params.car, {
+            include: [
+                {
+                    model: Location,
+                    attributes: [
+                        'id',
+                        'city',
+                        'airport',
+                        'embed'
+                    ]
+                },
+            ],
+        });
+
+        const carChoice = carData.get({ plain: true });
+        const cities = carChoice.locations;
+        res.render('booknow', { cities, cars, carChoice, logged_in: req.session.logged_in });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/choice/:city/:car', withAuth, async (req, res) => {
     try {
         const bookData = await Location.findByPk(req.params.city, {
             include: [
