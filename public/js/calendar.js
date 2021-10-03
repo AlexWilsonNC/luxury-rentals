14f54f69e35b2a7f92558194ef6a08a698df666f
Jack Jac <k.a.jack21@gmail.com>
2021-10-02 22:16:51 -0400

    got datepicker working and added to html

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,16 +1,32 @@
-var daysInMonth = ["1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31"];
-var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+//import Datepicker from './Datepicker.js';
 
-function getDaysInMonth(month, year) {
+
+//var daysInMonth = ["1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31"];
+//var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const elem = document.getElementById('foo');
+const rangepicker = new DateRangePicker(elem, {
+  // ...options
+}); 
+
+
+//const elem = document.querySelector('input[name="calendar"]');
+//const datepicker = new Datepicker(elem, {
+  //Date: "mm/dd/yyy"
+//}); 
+
+console.log(elem);
+
+//function getDaysInMonth(month, year) {
  
-}
+//}
 
 // does something when a date is clicked
-reservation_window.frmCalendar.submit();
-function dateClicked(day, month, year) {
-    document.forms.calendar.date.value = day + '/' + month + '/' + year;
+//reservation_window.frmCalendar.submit();
+//function dateClicked(day, month, year) {
+  //  document.forms.calendar.date.value = day + '/' + month + '/' + year;
 
-}
+//}
 
 /* var settings = {
   DateTimeShow: true,
