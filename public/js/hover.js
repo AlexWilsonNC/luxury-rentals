ce73c0779ae09cbc21a47a481d1508470090eee0
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-10-03 18:39:13 -0400

    hover over card triggers additional hover effect through js

--- public/js/hover.js
+++ public/js/hover.js
@@ -1,13 +1,14 @@
-let card = document.getElementsByClassName('card');
+const card = document.querySelectorAll('.card');
 let imgOverlay = document.getElementsByClassName('image-overlay');
-
-card.onmouseover = () => {
-    imgOverlay.style.opacity = '1';
-}
-
-card.onmouseleave = () => {
-    imgOverlay.style.opacity = '0';
-}
-
-card.forEach(onmouseover);
-card.forEach(onmouseleave);
+card.forEach(function (el) {
+    el.addEventListener("mouseover", function () {
+        const parent = el.querySelector('.image');
+        const imgOverlay = parent.querySelector('.image-overlay');
+        imgOverlay.style.opacity = '1';
+    })
+    el.addEventListener("mouseleave", function () {
+        const parent = el.querySelector('.image');
+        const imgOverlay = parent.querySelector('.image-overlay');
+        imgOverlay.style.opacity = '0';
+    })
+});
