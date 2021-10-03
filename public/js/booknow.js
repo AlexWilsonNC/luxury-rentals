a24455e4ad4cda89a1ac3bd011508373ebff3dc9
Keri Price <keri.lynne.p@gmail.com>
2021-10-03 14:59:25 -0400

    location selector includes correct car list

--- public/js/booknow.js
+++ public/js/booknow.js
@@ -2,21 +2,18 @@ const confirmBtnEl = document.querySelector('.confirm-btn');
 const locationSelector = document.querySelector('#location-selector');
 const carSelector = document.querySelector('#vehicle-selector');
 
-const charlotteCars = [];
-const ferrari = [];
-
 const defaultSelector = () => {
   const city = window.location.pathname.split('/')[2];
   const car = window.location.pathname.split('/')[3];
 
   console.log(city, car);
-  for (var i, j = 0; i = locationSelector.options[j]; j++) {
+  for (let i, j = 0; i = locationSelector.options[j]; j++) {
     if (i.value == city) {
       locationSelector.selectedIndex = j;
       break;
     };
   };
-  for (var i, j = 0; i = carSelector.options[j]; j++) {
+  for (let i, j = 0; i = carSelector.options[j]; j++) {
     if (i.value == car) {
       carSelector.selectedIndex = j;
       break;
@@ -25,13 +22,44 @@ const defaultSelector = () => {
 };
 
 const selectCityHandler = () => {
-  console.log(locationSelector.value);
-  if(parseInt(locationSelector.value) === 0) {
-    console.log("set all to display");
+  const locId = locationSelector.value;
+  if (locId === '0') {
+    for (let i = 1; i < carSelector.options.length; i++) {
+      let selected = document.querySelector(`#car-${carSelector.options[i].value}`);
+      selected.classList.remove('hidden');;
+    };
     return;
   };
 
-  // fetch request to get car ids by location id
+  fetch(`/api/location/${locId}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+
+      let stock = [];
+      for (let i = 0; i < data.cars.length; i++) {
+        stock.push(data.cars[i].id);
+      };
+
+      for (let j = 1; j < carSelector.options.length; j++) {
+        let selected = document.querySelector(`#car-${carSelector.options[j].value}`);
+        if (!stock.includes(parseInt(carSelector.options[j].value))) {
+          selected.classList.add('hidden');
+        } else {
+          selected.classList.remove('hidden');
+        }
+      };
+
+      if (carSelector.options[carSelector.selectedIndex].classList.contains('hidden')) {
+        carSelector.selectedIndex = 0;
+      };
+
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
 };
 
 const bookNowFormHandler = async (event) => {
