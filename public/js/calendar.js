bc1538d0cf1bd2176b8fba678b22ddeb7ff5c9e6
Jack Jac <k.a.jack21@gmail.com>
2021-10-04 10:13:29 -0400

    removed cdatepicker function

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -4,10 +4,8 @@
 //var daysInMonth = ["1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31"];
 //var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const elem = document.getElementById('foo');
-const rangepicker = new DateRangePicker(elem, {
-  // ...options
-}); 
+
+
 
 
 //const elem = document.querySelector('input[name="calendar"]');
@@ -15,10 +13,10 @@ const rangepicker = new DateRangePicker(elem, {
   //Date: "mm/dd/yyy"
 //}); 
 
-console.log(elem);
+//console.log(elem);
 
 //function getDaysInMonth(month, year) {
- 
+
 //}
 
 // does something when a date is clicked
